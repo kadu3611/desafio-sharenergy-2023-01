5fd8e9b2a2c267e72dd992cafbaa8ea6b09854b8
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-10 21:44:03 -0300

    feito CRUD mongo e testado.

--- back-end/src/controller/user.controller.ts
+++ back-end/src/controller/user.controller.ts
@@ -7,7 +7,45 @@ const userController = {
 
         return res.json(users);
 
-    }
+    },
+
+    async findById(req:Request, res: Response): Promise<Response>{
+        const { id } = req.params;
+
+        const userId = await userModel.findById(id);
+
+        return res.json(userId);
+
+    },
+
+    async create(req:Request, res: Response): Promise<Response>{
+  
+        const newUser = await userModel.create(req.body);
+
+        return res.json(newUser);
+
+    },
+    async update(req:Request, res: Response): Promise<Response>{
+
+        const { id } = req.params;
+  
+        const deleteUser = await userModel.findByIdAndUpdate(id, req.body);
+
+        return res.json(deleteUser);
+
+    },
+
+    async delete(req:Request, res: Response): Promise<Response>{
+
+        const { id } = req.params;
+  
+        const deleteUser = await userModel.findByIdAndDelete(id);
+
+        return res.json(deleteUser);
+
+    },
+
+
 }
 
 export default userController;
--- back-end/src/routes/router.ts
+++ back-end/src/routes/router.ts
@@ -6,5 +6,13 @@ const router = Router();
 
 router.get("/users", userController.find);
 
+router.get("/users/:id", userController.findById);
+
+router.post("/users", userController.create);
+
+router.put("/users", userController.update);
+
+router.delete("/users/:id", userController.delete);
+
 
 export default router;
