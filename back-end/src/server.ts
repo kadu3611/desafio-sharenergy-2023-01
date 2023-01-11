ca7a026e1b1705c64a498cc9e72bab4f9cf45ee8
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-10 21:06:02 -0300

    instalado outras dependências, feito teste com express e 'ok'

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 // import router from './routes/router';
 
 
 const app = express()
 
-app.use(express.json());
+// app.use(express.json());
 
 // app.use(router)
+
+app.get('/', (req:Request, res:Response) => {
+    return res.send('Helo!')
+})
+
+app.listen(3000)
