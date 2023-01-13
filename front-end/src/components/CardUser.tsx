3caae96ae125a621fc4dc1de6b823d803d774c5f
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-13 14:40:15 -0300

    Feito card com nome, email, imagem e idade

---
+++ front-end/src/components/CardUser.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+interface Props {
+    items: {
+        picture: {
+            large: string
+        },
+        name:{
+            first: string,
+            last: string,
+        },
+        email:string,
+        login:{
+            username:string,
+        },
+        registered:{
+            age:number
+        }
+    }[];
+}
+
+    //foto do usuário, nome completo, email, username e idade
+
+
+const CardUser: React.FC<Props> = ({ items }) => {
+    return (
+        <div>
+            {items.map((item, index) => (
+                <div key={index}>
+                    <img
+                    src={item.picture.large} 
+                    alt="Foto do usuário"
+                    />
+                        <div>
+                            {`Nome completo: ${item.name.first} ${item.name.last}`}
+                        </div>
+                        <div>
+                            {`Email: ${item.email}`}
+                        </div>
+                        <div>
+                            {`Username: ${item.login.username}`}
+                        </div>
+                        <div>
+                            {`Idade: ${item.registered.age}`}
+                        </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default CardUser;
+
+
+
+
--- front-end/src/pages/RandomUserGeneration.tsx
+++ front-end/src/pages/RandomUserGeneration.tsx
@@ -1,19 +1,54 @@
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, {  useCallback, useContext, useEffect, useState } from 'react';
+import CardUser from '../components/CardUser';
 import ContextComponents from '../context/ContextComponents';
 
 function RandomUserGeneration() {
 
-  const { contextComponents } = useContext(ContextComponents);
-  const { username, password, setUsername, setPassword,
-     checkEnable, setCheckEnable } = contextComponents
+    // const { contextComponents } = useContext(ContextComponents);
+    // const { username, password, setUsername, setPassword,
+    //     checkEnable, setCheckEnable } = contextComponents
+    const [ arrayApi, setArrayApi ] = useState([])
+    const [ filterApi, setFilterApi ] = useState([])
 
 
-  return (
-    <div>
-      RandomUserGeneration
+    const apiUser =  useCallback(async() => {
+        try {
+            const response =
+                await fetch('https://randomuser.me/api/?page=3&results=4');
+            const data = await response.json();
+            console.log(data.results, 'data');
+            setArrayApi(data.results)   
+            
+        } catch (e) {
+            console.log(e);
+        }
+    }, [])
 
-    </div>
-  );
+    const getFilter = (event:any):void => {
+        const { target } = event
+        const { value } = target
+        const arrayCloneApi = arrayApi
+        const arrayFilter = arrayCloneApi
+        // .filter((elemento) => elemento.includes(value))
+    }
+
+
+    useEffect(() => {
+        apiUser()
+    },[apiUser])
+
+    
+    return (
+        <div>
+            <input type="text"
+            onChange={getFilter}
+            />
+            <CardUser
+            items = {arrayApi}
+            />
+
+        </div>
+    );
 }
 
 export default RandomUserGeneration;
