0fd607db7defbef0bcedea0b01007c2bb04f25e5
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-14 21:44:36 -0300

    Feito campo de busca e tela de Loading...

---
+++ front-end/src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+    //foto do usuário, nome completo, email, username e idade
+
+
+const Loading:React.FC = () => {
+    return (
+        <div>
+            Loading ...
+
+        </div>
+    );
+};
+
+export default Loading;
+
+
+
+
--- front-end/src/pages/RandomUserGeneration.tsx
+++ front-end/src/pages/RandomUserGeneration.tsx
@@ -1,50 +1,63 @@
-import React, {  useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import CardUser from '../components/CardUser';
+import Loading from '../components/Loading';
 import ContextComponents from '../context/ContextComponents';
 
 function RandomUserGeneration() {
 
-    // const { contextComponents } = useContext(ContextComponents);
-    // const { username, password, setUsername, setPassword,
-    //     checkEnable, setCheckEnable } = contextComponents
-    const [ arrayApi, setArrayApi ] = useState([])
-    const [ filterApi, setFilterApi ] = useState([])
+    interface ItemFilter {
+        name: {
+            first: string,
+            last: string,
+        },
+        email: string,
+        login: {
+            username: string,
+        },
+    }
+    const [arrayApi, setArrayApi] = useState([])
+    const [filterApi, setFilterApi] = useState([])
 
 
-    const apiUser =  useCallback(async() => {
+    const apiUser = useCallback(async () => {
         try {
             const response =
                 await fetch('https://randomuser.me/api/?page=3&results=4');
             const data = await response.json();
-            console.log(data.results, 'data');
-            setArrayApi(data.results)   
-            
+            setArrayApi(data.results)
+            setFilterApi(data.results)
+
         } catch (e) {
             console.log(e);
         }
     }, [])
 
-    const getFilter = (event:any):void => {
+    const getFilter = (event: any): void => {
         const { target } = event
         const { value } = target
-        const arrayCloneApi = arrayApi
-        const arrayFilter = arrayCloneApi
-        // .filter((elemento) => elemento.includes(value))
+        const fullNames = arrayApi
+            .filter((items: ItemFilter) => (
+                `${items.name.first} ${items.name.last}
+                 ${items.email} ${items.login.username}`)
+                .includes(value))
+        setFilterApi(fullNames)
     }
 
 
     useEffect(() => {
         apiUser()
-    },[apiUser])
+    }, [apiUser])
 
-    
+
     return (
+        filterApi.length === 0 ? <Loading/> : 
         <div>
             <input type="text"
-            onChange={getFilter}
+                onChange={getFilter}
+                placeholder="Pesquisar"
             />
             <CardUser
-            items = {arrayApi}
+                items={filterApi}
             />
 
         </div>
